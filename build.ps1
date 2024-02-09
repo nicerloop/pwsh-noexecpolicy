a14b13d6215a53dbcadade4343f414d6cbd38c24
Loïc PÉRON <loic.peron@gmail.com>
2024-02-09 11:38:28 +0100

    build: restore accidentally commented instructions

--- build.ps1
+++ build.ps1
@@ -1,22 +1,22 @@
 # Write-Host "Get PowerShell sources for version 7.4.1"
-# git clone https://github.com/PowerShell/PowerShell.git
+git clone https://github.com/PowerShell/PowerShell.git
 Set-Location PowerShell
-# git checkout v7.4.1
-# git reset
-# git clean -xdf
+git checkout v7.4.1
+git reset
+git clean -xdf
 
 # Write-Host "Disable ExecutionPolicy"
-# $secFile="src/System.Management.Automation/security/SecuritySupport.cs"
-# $oldFile="$secFile.orig"
-# if (!(Test-Path $oldFile)) {
-#     Move-Item $secFile $oldFile
-# } else {
-#     Remove-Item $secFile
-# }
-# Get-Content $oldFile | ForEach-Object { $_ -replace "#if UNIX", "#if !UNIX" } | Set-Content $secFile
+$secFile="src/System.Management.Automation/security/SecuritySupport.cs"
+$oldFile="$secFile.orig"
+if (!(Test-Path $oldFile)) {
+    Move-Item $secFile $oldFile
+} else {
+    Remove-Item $secFile
+}
+Get-Content $oldFile | ForEach-Object { $_ -replace "#if UNIX", "#if !UNIX" } | Set-Content $secFile
 
-# Write-Host "Compile PowerShell"
-# # https://github.com/PowerShell/PowerShell/blob/master/docs/building/windows-core.md
-# Import-Module ./build.psm1
-# Start-PSBuild
+Write-Host "Compile PowerShell"
+# https://github.com/PowerShell/PowerShell/blob/master/docs/building/windows-core.md
+Import-Module ./build.psm1
+Start-PSBuild
 Compress-Archive -Path src/powershell-win-core/bin/Debug/net8.0/win7-x64/publish/* -DestinationPath C:/vagrant/PowerShell-NoExecPolicy-7.4.1-win-x64.zip -Force
