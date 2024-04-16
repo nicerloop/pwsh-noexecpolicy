4fcedfd06989f6a501933dece0f9f845eb183d83
Loïc PÉRON <loic.peron@gmail.com>
2024-04-16 15:50:06 +0200

    disable progress during setup

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,9 @@
+# Prevent issues with CLI non-interactive execution
+# https://github.com/PowerShell/Microsoft.PowerShell.Archive/issues/77#issuecomment-601947496
+Write-Host "Disable progress bars for CLI non-interactive execution"
+$ProgressPreference = "SilentlyContinue"
+$global:ProgressPreference = "SilentlyContinue"
+
 Write-Host "Install Scoop (https://scoop.sh)"
 if (Get-Command -ErrorAction SilentlyContinue scoop) {
     Write-Host "Scoop already installed"
