4ae75e34b40d57d50d0f903aba54bacd3fdabdd4
Loïc PÉRON <loic.peron@gmail.com>
2024-04-16 15:50:41 +0200

    set $ReleaseTag

--- build.ps1
+++ build.ps1
@@ -18,5 +18,5 @@ Get-Content $oldFile | ForEach-Object { $_ -replace "#if UNIX", "#if !UNIX" } | 
 Write-Host "Compile PowerShell"
 # https://github.com/PowerShell/PowerShell/blob/master/docs/building/windows-core.md
 Import-Module ./build.psm1
-Start-PSBuild
+Start-PSBuild -ReleaseTag v7.4.2-noexecpolicy
 Compress-Archive -Path src/powershell-win-core/bin/Debug/net8.0/win7-x64/publish/* -DestinationPath C:/vagrant/PowerShell-NoExecPolicy-7.4.2-win-x64.zip -Force
