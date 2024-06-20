2bb7e2532b995eea36bea1168db107fd800fd47a
Loïc PÉRON <loic.peron@gmail.com>
2024-06-20 08:18:21 +0200

    parameterize PowerShell version

--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
-Write-Host "Get PowerShell sources for version 7.4.2"
+$PowerShellVersion="7.4.2"
+Write-Host "Get PowerShell sources for version $PowerShellVersion"
 git clone https://github.com/PowerShell/PowerShell.git
 Set-Location PowerShell
-git checkout v7.4.2
+git checkout v$PowerShellVersion
 git reset
 git clean -xdf
 
@@ -18,5 +19,5 @@ Get-Content $oldFile | ForEach-Object { $_ -replace "#if UNIX", "#if !UNIX" -rep
 Write-Host "Compile PowerShell"
 # https://github.com/PowerShell/PowerShell/blob/master/docs/building/windows-core.md
 Import-Module ./build.psm1
-Start-PSBuild -ReleaseTag v7.4.2
-Compress-Archive -Path src/powershell-win-core/bin/Debug/net8.0/win7-x64/publish/* -DestinationPath C:/vagrant/PowerShell-NoExecPolicy-7.4.2-win-x64.zip -Force
+Start-PSBuild -ReleaseTag v$PowerShellVersion
+Compress-Archive -Path src/powershell-win-core/bin/Debug/net8.0/win7-x64/publish/* -DestinationPath C:/vagrant/PowerShell-NoExecPolicy-$PowerShellVersion-win-x64.zip -Force
