64476db746dbd3240e08991e0ce2e2808fa54e12
Loïc PÉRON <loic.peron@gmail.com>
2025-05-01 18:28:27 +0200

    update to PowerShell 7.5.1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$PowerShellVersion="7.5.0"
+$PowerShellVersion="7.5.1"
 Write-Host "Get PowerShell sources for version $PowerShellVersion"
 git clone https://github.com/PowerShell/PowerShell.git
 Set-Location PowerShell
